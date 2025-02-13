a068e7a5e13e506f18becc0146621e4b7d9e5678
Leah Queen <leah@codegeek.net>
2025-02-13 14:00:50 -0700

    Adds utility nav and styles dropdown menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/header.css';
-// import logo from '../assets/images/logo-lrg-300x54-1.svg';
 
 function HeaderNav() {
     const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -12,40 +11,55 @@ function HeaderNav() {
     };
 
     return (
+        <nav className="utility-nav">
+            <div className="utility-wrapper">
+                <ul className="utility-links">
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
+            </div>
+
         <nav className="navbar-container" role="navigation">
             <div className="navbar-wrapper">
-                {/* <div className="navbar-left">
-                    <Link to="/home" className="navbar-logo">
-                        <img src={logo} alt="Logo" className="logo-img" />
-                    </Link>
-                </div> */}
                 <div className="navbar-center">
                     <ul className="navbar-links">
                         <li>
                             <Link to="/home">Home</Link>
                         </li>
+
                         <li className="dropdown">
-                            <button onClick={toggleDropdown} className="dropdown-button">Campsites</button>
+                            <button
+                                className={`dropdown-button ${isDropdownOpen ? 'open' : ''}`}
+                                onClick={toggleDropdown}
+                                aria-haspopup="true"
+                                aria-expanded={isDropdownOpen ? "true" : "false"}
+                            >
+                                Campsites
+                            </button>
+
                             {/* Dropdown menu */}
-                            {isDropdownOpen && (
-                                <ul className="dropdown-menu">
-                                    <li><Link to="/posts">All Posts</Link></li>
-                                    <li><Link to="/post/75">Test</Link></li>
-                                    <li><Link to="/post/14">Poudre River</Link></li>
-                                    <li><Link to="/post/12">Yellowstone</Link></li>
-                                </ul>
-                            )}
+                            <ul className="dropdown-menu">
+                                <li><Link to="/posts">All Posts</Link></li>
+                                <li><Link to="/post/75">Test</Link></li>
+                                <li><Link to="/post/14">Poudre River</Link></li>
+                                <li><Link to="/post/12">Yellowstone</Link></li>
+                            </ul>
                         </li>
+
                         <li>
                             <Link to="/about">About</Link>
                         </li>
                     </ul>
                 </div>
+
                 <div className="navbar-right">
                     {/* Add login functionality here */}
                 </div>
             </div>
+            </nav>
         </nav>
+
     );
 }
 
